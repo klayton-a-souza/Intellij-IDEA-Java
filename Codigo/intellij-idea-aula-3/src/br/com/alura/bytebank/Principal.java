a79fd801090dbd9435c5012ce59f0c4fd4716611
klayton-a-souza <klayton.al.souza@gmail.com>
2022-11-09 09:46:51 -0300

    Codigo 6.1: Utilizando tamplates

--- Codigo/intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
+++ Codigo/intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
@@ -1,10 +1,29 @@
 package br.com.alura.bytebank;
 
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
 import br.com.alura.bytebank.visualizador.OpcoesPagamento;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Principal {
-    public static void main(String[] args) {
-        new OpcoesPagamento().mensagemDeBoasVindas();
+    public static void main(String[] args) throws IOException {
+//        new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentoList = leitor.ler(1);
+
+        pagamentoList.forEach(pagamento -> {
+
+            double valorBruto = pagamento.getValor();
+
+            if (valorBruto > 100) {
+                double valorAcrecimo = valorBruto * 1.1;
+                pagamento.setValor(valorAcrecimo);
+            }
+            System.out.println(pagamento.getValor());
+        });
     }
 
 }
