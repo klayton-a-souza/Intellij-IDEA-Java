5187dc67629da2455e3d5b29857498d2a89a9855
klayton-a-souza <klayton.al.souza@gmail.com>
2022-11-09 09:11:29 -0300

    Codigo 5.1: Criando testes com JUnit

---
+++ Codigo/intellij-idea-aula-3/src/br/com/alura/bytebank/io/test/LeitorCSVTest.java
@@ -0,0 +1,35 @@
+package br.com.alura.bytebank.io.test;
+
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeitorCSVTest {
+
+    @Test
+    void pagamentosDeAbril() throws IOException {
+        LeitorCSV leitorCSV = new LeitorCSV();
+        List< Pagamento> pagamentos = leitorCSV.ler(1);
+
+        assertEquals(5,pagamentos.size());
+    }
+    @Test
+    void pagamentosDeMaio() throws IOException {
+        LeitorCSV leitorCSV = new LeitorCSV();
+        List< Pagamento> pagamentos = leitorCSV.ler(3);
+
+        assertEquals(5,pagamentos.size());
+    }
+    @Test
+    void pagamentosDeJulho() throws IOException {
+        LeitorCSV leitorCSV = new LeitorCSV();
+        List< Pagamento> pagamentos = leitorCSV.ler(2);
+
+        assertEquals(5,pagamentos.size());
+    }
+}
